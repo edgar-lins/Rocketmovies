e48eabed45759606a7f6894b833f4336da45d5c0
Edgar Lins <edgarlins.dev@gmail.com>
2024-07-16 15:39:36 -0300

    Class: Controllers

---
+++ api/src/controllers/UsersController.js
@@ -0,0 +1,13 @@
+class UsersController {
+    /**
+     * index - GET para listar vários registros.
+     * show - GET para exibir um registro específico.
+     * create - POST para criar um registro.
+     * update - PUT para atualizar um registro.
+     * delete - DELELE para remover um registro.
+     */
+
+    
+};
+
+module.exports = UsersController;
