07f981983e74ec910fd31541eb651190f1b98f14
Edgar Lins <edgarlins.dev@gmail.com>
2024-07-24 11:26:19 -0300

    Class: Criptografando senha do usuário

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,3 +1,4 @@
+const { hash } = require("bcryptjs"); 
 const AppError = require("../utils/AppError");
 
 const sqliteConnection = require('../database/sqlite');
@@ -21,9 +22,11 @@ class UsersController {
             throw new AppError("Este e-mail já está em uso.");
         }
 
+        const hashedPassword = await hash(password, 8)
+
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, password]
+            [name, email, hashedPassword]
         );
 
         return response.status(201).json()
