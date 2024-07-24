27c59429e2d78fbc511cff2c8d0d7a295c86f8be
Edgar Lins <edgarlins.dev@gmail.com>
2024-07-24 12:21:47 -0300

    Class: Atualizando usuário - Server Error

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,6 +1,5 @@
 const { hash } = require("bcryptjs"); 
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require('../database/sqlite');
 
 class UsersController {
@@ -31,6 +30,38 @@ class UsersController {
 
         return response.status(201).json()
     } 
+
+    async update(request, response) {
+        const { name, email } = request.body;
+        const { id } = request.params;
+
+        const database = await sqliteConnection();
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+        if(!user) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at ?
+            WHERE id = ?`,
+            [user.name, user.email, new Date(), id]
+        );
+
+        return response.status(200).json();
+    }
 };
 
 module.exports = UsersController;
