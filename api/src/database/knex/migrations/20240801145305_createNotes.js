002ccb7a80a68572024412a61fabe00cdc50a8a0
Edgar Lins <edgarlins.dev@gmail.com>
2024-08-12 12:00:29 -0300

    Migrations para Links e Tags

--- api/src/database/knex/migrations/20240801145305_createNotes.js
+++ api/src/database/knex/migrations/20240801145305_createNotes.js
@@ -9,4 +9,4 @@ exports.up = knex => knex.schema.createTable("movie_notes", table => {
     table.timestamp("updated_at").default(knex.fn.now());
 });
 
-exports.down = knex => knex.schema.dropTable("notes");
+exports.down = knex => knex.schema.dropTable("movie_notes");
