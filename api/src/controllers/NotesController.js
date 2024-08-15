df28e27f36d0365b4e51c3d0d40f9fbcb270ac29
Edgar Lins <edgarlins.dev@gmail.com>
2024-08-15 16:22:20 -0300

    Class: Deletando nota

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -45,6 +45,14 @@ class NotesController {
             tags
         })
     }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("movie_notes").where({ id }).delete();
+
+        return response.json();
+    }
 }
 
 module.exports = NotesController;
