625bfedd8d49ea31f83f772d137b3c951bcc38a3
Edgar Lins <edgarlins.dev@gmail.com>
2024-08-15 16:17:45 -0300

    Class: Exibindo nota

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -33,6 +33,18 @@ class NotesController {
 
         response.json();
     }
+
+    async show(request, response) {
+        const { id } = request.params;
+
+        const note = await knex("movie_notes").where({ id }).first();
+        const tags = await knex("movie_tags").where({ note_id: id }).orderBy("name");
+
+        return response.json({
+            ...note,
+            tags
+        })
+    }
 }
 
 module.exports = NotesController;
