96025ef59808b920544660af238fefbda044af40
Edgar Lins <edgarlins.dev@gmail.com>
2024-08-19 09:58:00 -0300

    Class: Aplicando InnerJoin

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -63,7 +63,16 @@ class NotesController {
             const filterTags = tags.split(',').map(tag => tag.trim());
 
             notes = await knex("movie_tags")
+            .select([
+                "movie_notes.id",
+                "movie_notes.title",
+                "movie_notes.user_id"
+            ])
+            .where("movie_notes.user_id", user_id)
+            .whereLike("movie_notes.title", `%${title}%`)
             .whereIn("name", filterTags)
+            .innerJoin("movie_notes", "movie_notes.id", "movie_tags.note_id")
+            .orderBy("movie_notes.title")
 
         } else {
             notes = await knex("movie_notes")
