6609ffd8a461d65c836fb7e3551bda455cddbf14
Edgar Lins <edgarlins.dev@gmail.com>
2024-08-19 09:38:08 -0300

    Class: Filter WhereIn

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -55,12 +55,22 @@ class NotesController {
     }
 
     async index(request, response) {
-        const { title, user_id } = request.query;
+        const { title, user_id, tags } = request.query;
 
-        const notes = await knex("movie_notes")
-        .where({ user_id })
-        .whereLike("title", `%${title}%`)
-        .orderBy("title")
+        let notes;
+
+        if(tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("movie_tags")
+            .whereIn("name", filterTags)
+
+        } else {
+            notes = await knex("movie_notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy("title")
+        }
 
         return response.json({ notes });
     }
